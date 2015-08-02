b4cbe9ac3afeb51dc6df91b505ab323d8d3a157c
Lucius <lucius@simple.com>
2015-08-02 00:06:44 -0700

    Major Changes

--- install-vmware-tools.ps1
+++ install-vmware-tools.ps1
@@ -1,6 +1,21 @@
-# from /Applications/VMware Fusion.app/Contents/Library/isoimages/windows.iso
+# Set the path of the VMWare Tools ISO - this is set in the Packer JSON file
 
-&"c:\7-zip\7z.exe" x "C:\Windows\Temp\windows.iso" -oc:\windows\temp\vmware -aoa | Out-Host
-&C:\Windows\Temp\vmware\setup64.exe /S /v`"/qn REBOOT=R`" | Out-Host
+$isopath = "C:\Windows\Temp\windows.iso"
 
-exit 0
+# Mount the .iso, then build the path to the installer by getting the Driveletter attribute from Get-DiskImage piped into Get-Volume and adding a :\setup64.exe
+# A separate variable is used for the parameters. There are cleaner ways of doing this. I chose the /qr MSI Installer flag because I personally hate silent installers
+# Even though our build is headless. 
+
+
+Mount-DiskImage -ImagePath $isopath
+$exe = ((Get-DiskImage -ImagePath $isopath | Get-Volume).Driveletter + ':\setup64.exe')
+$parameters = '/S /v "/qr REBOOT=R"'
+
+# Now we execute the process with Start-Process, which lets the script wait until the process is complete. 
+
+Start-Process $exe $parameters -Wait
+
+#Time to clean up - dismount the image and delete the original ISO
+
+Dismount-DiskImage -ImagePath $isopath
+Remove-Item $isopath
